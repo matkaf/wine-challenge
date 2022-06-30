adc2fa8af4801dff071446cbe99256dc7d50ee13
Matheus Kaffka <matheus.kaffka@gmail.com>
2022-06-29 23:38:42 -0300

    chore: implementa SSR e estilo global

---
+++ src/pages/_document.tsx
@@ -0,0 +1,44 @@
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document'
+
+import { ServerStyleSheet } from 'styled-components'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: [initialProps.styles, sheet.getStyleElement()],
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang='pt'>
+        <Head>
+          <meta charSet='utf-8' />
+
+          <link rel='icon' href='https://img.wine.com.br/fenix/image/_big_bang/icons/favicon/favicon.ico' />
+
+          <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap" rel="stylesheet" /> 
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+const theme = {
+  colors: {
+    background: '#DEDEDE',
+    title: '#123952',
+    text: '#617480',
+    subtitle: '#A0ACB3',
+    button: '#51B853',
+  }
+}
+
+export default theme
