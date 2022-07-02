20678ce4020831b928c2ac09920b793ec127d75a
Matheus Kaffka <matheus.kaffka@gmail.com>
2022-07-02 20:06:00 -0300

    feat: adiciona pre-render a pagina

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,26 @@
-import type { NextPage } from 'next'
+import type { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next'
 import Head from 'next/head'
 
 import Header from '../components/Header'
 import WineList from '../components/WineList'
 
-const Home: NextPage = () => {
+import { IAPIRes } from '../interfaces'
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch('https://wine-back-test.herokuapp.com/products?page=1&limit=9')
+
+  const wines: IAPIRes = await res.json()
+
+  console.log(wines);
+
+  if (!wines) return { notFound: true }
+
+  return {
+    props: { wines }
+  }
+}
+
+const Home: NextPage =  ({ wines }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <>
       <Head>
@@ -12,7 +28,7 @@ const Home: NextPage = () => {
       </Head>
       <Header />
       <main>
-        <WineList />
+        <WineList wines={wines}/>
       </main>
     </>
   )
