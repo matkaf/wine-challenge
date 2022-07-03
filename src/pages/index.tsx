f184aeffbab426f3eb3aef6dbb306575015af48e
Matheus Kaffka <matheus.kaffka@gmail.com>
2022-07-03 01:07:22 -0300

    refactor: deixa de usar pre-rendering do next e passa a usar o contexto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import React, { useContext, useEffect } from 'react'
 import type { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next'
 import Head from 'next/head'
 
@@ -5,20 +6,37 @@ import Header from '../components/Header'
 import WineList from '../components/WineList'
 
 import { IAPIRes } from '../interfaces'
+import AppContext from '../context/AppContext'
+import { getWines } from '../api'
 
-export const getStaticProps: GetStaticProps = async () => {
-  const res = await fetch('https://wine-back-test.herokuapp.com/products?page=1&limit=9')
+// export const getStaticProps: GetStaticProps = async () => {
+//   const res = await fetch('https://wine-back-test.herokuapp.com/products?page=1&limit=9')
 
-  const wines: IAPIRes = await res.json()
+//   const wines: IAPIRes = await res.json()
 
-  if (!wines) return { notFound: true }
+//   if (!wines) return { notFound: true }
 
-  return {
-    props: { wines }
-  }
-}
+//   return {
+//     props: { wines }
+//   }
+// }
 
-const Home: NextPage =  ({ wines }: InferGetStaticPropsType<typeof getStaticProps>) => {
+const Home: NextPage =  (/*{ wines }: InferGetStaticPropsType<typeof getStaticProps>*/) => {
+  const { filteredWines, setFilteredWines } = useContext(AppContext)
+
+  useEffect(() => {
+    const fillContext = async () => {
+      const wines = await getWines(1)
+      setFilteredWines(wines)
+      console.log('Context alimentado!');
+      console.log(wines);
+    }
+
+    fillContext()
+  }, [])
+
+  // if (!filteredWines) return <h1>Carregando</h1>
+
   return (
     <>
       <Head>
@@ -26,7 +44,7 @@ const Home: NextPage =  ({ wines }: InferGetStaticPropsType<typeof getStaticProp
       </Head>
       <Header />
       <main>
-        <WineList wines={wines}/>
+        {filteredWines && <WineList filteredWines={filteredWines}/>}
       </main>
     </>
   )
