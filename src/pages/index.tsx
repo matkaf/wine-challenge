d81ae4d8d89147e2f9644a078c0b4e04d1cdf0fa
Matheus Kaffka <matheus.kaffka@gmail.com>
2022-07-03 15:31:27 -0300

    style: deleta comentarios do código

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,14 @@
 import React, { useContext, useEffect } from 'react'
-import type { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next'
+import type { NextPage } from 'next'
 import Head from 'next/head'
 
 import Header from '../components/Header'
 import WineList from '../components/WineList'
 
-import { IAPIRes } from '../interfaces'
 import AppContext from '../context/AppContext'
 import { getWines } from '../api'
 
-// export const getStaticProps: GetStaticProps = async () => {
-//   const res = await fetch('https://wine-back-test.herokuapp.com/products?page=1&limit=9')
-
-//   const wines: IAPIRes = await res.json()
-
-//   if (!wines) return { notFound: true }
-
-//   return {
-//     props: { wines }
-//   }
-// }
-
-const Home: NextPage =  (/*{ wines }: InferGetStaticPropsType<typeof getStaticProps>*/) => {
+const Home: NextPage =  () => {
   const { filteredWines, setFilteredWines } = useContext(AppContext)
 
   useEffect(() => {
@@ -34,8 +21,6 @@ const Home: NextPage =  (/*{ wines }: InferGetStaticPropsType<typeof getStaticPr
 
     fillContext()
   }, [setFilteredWines])
-
-  // if (!filteredWines) return <h1>Carregando</h1>
 
   return (
     <>
